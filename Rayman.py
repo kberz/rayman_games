ee25ad5ac44a75357f7109a71aef317165cd7b1a
Souslapluie <77190496+Souslapluie@users.noreply.github.com>
2021-04-02 01:22:28 +0200

    Update Rayman.py

--- Rayman.py
+++ Rayman.py
@@ -2,11 +2,17 @@
 # Site pour avoir pleins de sprites = https://www.spriters-resource.com/
 
 import sys
-import pygame 
+import pygame
+from pygame.locals import *
 
 pygame.init()
-
 # Création d'une classe pour le joueur :
+
+
+"""  					La class ne marche pas pour l'instant, 
+				le sprite du perso a été injectée en dehors de la class
+				   				  Class = Objets 
+"""
 
 class Player(pygame.sprite.Sprite) :
 	def __init__(self) :
@@ -17,28 +23,32 @@ class Player(pygame.sprite.Sprite) :
 		self.max_health = 5
 		self.attack = 1
 		self.speed = 5
-		self.image = pygame.image.load('Persos/Joueur/NoRay.png') #Chargement de l'image du joueur
+		self.image = pygame.image.load('Persos/Joueur/NoRay/NoRay.png') #Chargement de l'image du joueur
 		self.rect = self.image.get_rect()
 		
 # Générer fenêtre de jeu :
 
 pygame.display.set_caption("Rayman Rev1val")  # display = affichage, set_caption = titre(s)
-screen = pygame.display.set_mode((1080, 720)) # set_mode = affichage de la fenêtre (taille)
+screen = pygame.display.set_mode((1280, 520)) # set_mode = affichage de la fenêtre (taille)
 
 # Importer images : 
 
-background = pygame.image.load('Background/LotLD.jpg') # Attribution d'une image à la variable 'background'
+background = pygame.image.load('Background/LotLD.jpg').convert() # Attribution d'une image à la variable 'background'
 
+# Application image arrière-plan ( affichage ) :
+screen.blit(background, (0, 0))
+
+perso1 = pygame.image.load('Persos/Joueur/NoRay/NoRay.png') # déclaration image du perso
 
 running = True # running = Nom de la boucle while ? While ne marche pas toute seule, booléen strict minimum
 
 while running == True :
 
-	# Application image arrière-plan ( affichage ) :
-	screen.blit(background, (0, 0))
+	#Affichage joueur :
+	screen.blit(pygame.transform.scale(perso1,(90,90)), (120,370))
 
-    #Affichage joueur :
-	screen.blit(Player.image, Player.rect)
+	# blit() = afficher image,         
+	#(pygame.transform.scale(NomImage, (Taille, blit)), (, convertion) = changer taille et 
 
 	#MAJ ecran :
 	pygame.display.flip()
